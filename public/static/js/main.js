f1e16a105ac71adc686a6aa52534faacd68b10e6
Omar Trejo <otrenav@gmail.com>
2018-03-25 04:15:16 -0600

    Parse CSVs and add table functionality

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,6 +1,14 @@
 
-var LABELS, LAT, LNG, HEADING, FOV, PITCH, LIST = [];
+/* jshint esversion: 6 */
+
+var DATA = [
+    ['Name', 0, 19.433387, -99.133041, 0, 0, 120, 'Comment'],
+    [null, null, null, null, null, null, null, null]
+];
+var LABELS, NAME, GROUP, LAT, LNG, HEADING, PITCH, FOV, COMMENT;
 var KEY = 'AIzaSyCbz1HhLFdtBKwDmhd68sWmLa-38r9j6Mo';
+var CURRENT_OBSERVATION = 0;
+var N_OBSERVATIONS = 0;
 
 var GOOGLE_STREET_VIEW_API = (
     'https://maps.googleapis.com/maps/api/streetview?&size=1280x720&key=' + KEY
@@ -8,40 +16,73 @@ var GOOGLE_STREET_VIEW_API = (
 var GOOGLE_VISION_API = (
     'https://vision.googleapis.com/v1/images:annotate?key=' + KEY
 );
+var GOOGLE_STREET_VIEW_HISTORICAL_API_PART_1 = (
+    'https://maps.googleapis.com/maps/api/js/' +
+        'GeoPhotoService.SingleImageSearch?' +
+        'pb=!1m5!1sapiv3!5sUS!11m2!1m1!1b0!2m4!1m2!3d'
+);
+var GOOGLE_STREET_VIEW_HISTORICAL_API_PART_2 = (
+    '!2d50!3m10!2m2!1sen!2sGB!9m1!1e2!11m4!1m3!1e2!2b1!3e2!' +
+        '4m10!1e1!1e2!1e3!1e4!1e8!1e6!5m1!1e2!6m1!1e2&callback=_xdc_._v2mub5'
+);
 
 var getOptions = function() {
     LABELS = [];
-    $('#table').html('');
+    $('#clasification-table').html('');
+    NAME = $('#form-name').val();
+    GROUP = $('#form-group').val();
     LAT = $('#form-lat').val();
     LNG = $('#form-lng').val();
     HEADING = $('#form-heading').val();
-    FOV = $('#form-fov').val();
     PITCH = $('#form-pitch').val();
+    FOV = $('#form-fov').val();
+    COMMENT = $('#form-comment').val();
+    if (CURRENT_OBSERVATION == DATA.length - 1) {
+        insertEmptyObservation();
+    }
+    updateDataForCurrentObservation();
+    updateDataTable();
     if (allOptions()) {
         updateImage();
     }
+};
+
+var insertEmptyObservation = function() {
+    DATA.push([null, null, null, null, null, null, null, null]);
+};
+
+var updateDataForCurrentObservation = function() {
+    DATA[CURRENT_OBSERVATION] = [
+        NAME, GROUP, LAT, LNG, HEADING, PITCH, FOV, COMMENT
+    ];
 };
 
 var googleStreetViewImageURI = function() {
     var url = GOOGLE_STREET_VIEW_API;
     url += `&location=${LAT},${LNG}`;
     url += `&heading=${HEADING}`;
-    url += `&fov=${FOV}`;
     url += `&pitch=${PITCH}`;
+    url += `&fov=${FOV}`;
     return url;
 };
 
+// var googleStreetViewHistoricalURI = function() {
+//     var url = GOOGLE_STREET_VIEW_HISTORICAL_API_PART_1;
+//     url += LAT + '!4d' + LNG;
+//     url += GOOGLE_STREET_VIEW_HISTORICAL_API_PART_2;
+//     console.log('----------');
+//     console.log(url);
+//     console.log('----------');
+//     return (url);
+// };
+
 var updateImage = function() {
     var imageURI = googleStreetViewImageURI();
-    // console.log('---------');
-    // console.log('Image URI');
-    // console.log(imageURI);
-    // printOptions();
     $('#gsv-image').attr('src', imageURI);
 };
 
 var allOptions = function() {
-    if (LAT && LNG && HEADING && FOV && PITCH) {
+    if (LAT && LNG && HEADING && PITCH && FOV) {
         return true;
     }
     return false;
@@ -52,7 +93,7 @@ var googleVisionAPI = function(imageURI) {
         method: 'POST',
         url: GOOGLE_VISION_API,
         data: googleVisionRequest(),
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
         success: processGoogleVisionResponse,
         processData: false,
         error: function(data, status, error) {
@@ -61,6 +102,7 @@ var googleVisionAPI = function(imageURI) {
             console.log(data);
             console.log(status);
             console.log(error);
+            console.log('!!!!!!!!!!!!!!!!!!!!!!!!');
         }
     });
 };
@@ -102,53 +144,161 @@ var processGoogleVisionResponse = function(resp) {
         );
     }
     tableHTML += '</table>';
-    $('#table').html(tableHTML);
+    $('#clasification-table').html(tableHTML);
 };
 
-var addToList = function() {
-    LIST.push(currentOptions());
-    $('#list').val(JSON.stringify(LIST));
-};
+// var googleStreetViewHistoricalAPI = function(lng, lat) {
+//     $.ajax({
+//         method: 'GET',
+//         url: googleStreetViewHistoricalURI(lng, lat),
+//         // data: googleVisionRequest(),
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Access-Control-Allow-Origin': '*'
+//         },
+//         success: processGoogleStreetViewHistoricalResponse,
+//         processData: false,
+//         error: function(data, status, error) {
+//             console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+//             console.log('ERROR: Google Street View Historical');
+//             console.log(data);
+//             console.log(status);
+//             console.log(error);
+//             console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+//         }
+//     });
+// };
 
-var currentOptions = function() {
-    var labels = [];
-    for (var i = 0; i < LABELS.length; i++) {
-        labels.push({
-            label: LABELS[i].description,
-            score: LABELS[i].score
-        });
-    }
-    return {
-        lat: LAT,
-        lng: LNG,
-        heading: HEADING,
-        fov: FOV,
-        pitch: PITCH,
-        labels: labels
-    };
-};
+// var processGoogleStreetViewHistoricalResponse = function(resp) {
+//     console.log('0000000000000');
+//     console.log(resp);
+//     console.log('0000000000000');
+// };
 
 var download = function() {
-    var data = 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(LIST));
+    var data = 'data:text/plain;charset=utf-8,' + encodeURIComponent(
+        Papa.unparse({
+            fields: [
+                'Name', 'Group', 'Latitude', 'Longitude',
+                'Heading', 'Pitch', 'FOV', 'Comment'
+            ],
+            data: DATA
+        })
+    );
     var a = document.createElement('a');
-    a.setAttribute('download', 'images.json');
+    a.setAttribute('download', 'images.csv');
     a.setAttribute('href', data);
     a.click();
 };
 
-var printOptions = function() {
-    console.log('------------');
-    console.log(LAT);
-    console.log(LNG);
-    console.log(HEADING);
-    console.log(FOV);
-    console.log(PITCH);
-    console.log('------------');
+var uploadInit = function() {
+    $("#file-upload").change(function() {
+        Papa.parse($(this).prop('files')[0], {
+	        config: { header: true },
+            error: function(err, file, inputElem, reason) {
+                console.log('Input file error:');
+                console.log(err);
+                console.log(reason);
+            },
+	        complete: parseCSVUpload
+        });
+    });
+};
+
+var parseCSVUpload = function(results) {
+    var data = results.data;
+    DATA = [];
+    for (var i = 1; i < data.length - 1; i++) {
+        DATA.push([
+            data[i][0],
+            data[i][1],
+            data[i][2],
+            data[i][3],
+            data[i][4] ? data[i][4] : 0,
+            data[i][5] ? data[i][5] : 0,
+            data[i][6] ? data[i][6] : 120,
+            data[i][7]
+        ]);
+    }
+    updateDataTable();
+};
+
+var observationTableHTML = function() {
+    N_OBSERVATIONS += 1;
+    return `
+        <th><button onclick='deleteObservation(${N_OBSERVATIONS - 1})'>Delete</button></th>
+        <th>${NAME}</th>
+        <th>${GROUP}</th>
+        <th>${LAT}</th>
+        <th>${LNG}</th>
+        <th>${HEADING}</th>
+        <th>${PITCH}</th>
+        <th>${FOV}</th>
+        <th>${COMMENT}</th>
+    `;
+};
+
+var addCurrentObservationToTable = function() {
+    $('#data-table tr:nth-child(' + CURRENT_OBSERVATION + 1 + ')').html(
+        observationTableHTML()
+    );
+};
+
+var updateDataTable = function() {
+    var d;
+    var rows = `
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Group</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Heading</th>
+            <th>Pitch</th>
+            <th>FOV</th>
+            <th>Comment</th>
+            <th>Delete</th>
+        </tr>
+    `;
+    for (var i = 0; i < DATA.length; i++) {
+        rows += '<tr>';
+        rows += `<td>${i + 1}</td>`;
+        for (var j = 0; j < DATA[i].length; j++) {
+            d = DATA[i][j] != null ? DATA[i][j] : '';
+            rows += `<td onclick='selectObservation(${i})'>${d}</td>`;
+        }
+        rows += `<td><span onclick='deleteObservation(${i})'>X</span></td>`;
+        rows += '</tr>';
+    }
+    rows += '<tr>';
+    $('#data-table').html(rows);
+};
+
+var deleteObservation = function(i) {
+    DATA.splice(i, 1);
+    updateDataTable();
+    CURRENT_OBSERVATION = null;
+};
+
+var selectObservation = function(i) {
+    CURRENT_OBSERVATION = i;
+    $('#clasification-table').html('');
+    $('#form-name').val(DATA[i][0]);
+    $('#form-group').val(DATA[i][1]);
+    $('#form-lat').val(DATA[i][2]);
+    $('#form-lng').val(DATA[i][3]);
+    $('#form-heading').val(DATA[i][4]);
+    $('#form-pitch').val(DATA[i][5]);
+    $('#form-fov').val(DATA[i][6]);
+    $('#form-comment').val(DATA[i][7]);
+    getOptions();
 };
 
 var init = function() {
     $('input').on('input', getOptions);
+    updateDataTable();
     getOptions();
+    uploadInit();
 };
 
 init();
